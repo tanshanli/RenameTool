adeff420d97b8e30807a9c6624d0d1fda5992121
tanshanli <tan_shanli@qq.com>
2020-05-05 18:44:53 +0800

    增加中文名替换

--- RenameTool/Extensions.cs
+++ RenameTool/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RenameTool
@@ -8,6 +9,19 @@ namespace RenameTool
         public static IEnumerable<string> TrimSplit(this string text, params char[] separator)
         {
             return text.Split(separator).Select(s => s.Trim());
+        }
+
+        /// <summary>
+        /// 首字母小写写
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string FirstCharToLower(this string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return input;
+            string str = input.First().ToString().ToLower() + input.Substring(1);
+            return str;
         }
     }
 }
--- RenameTool/Program.cs
+++ RenameTool/Program.cs
@@ -10,31 +10,57 @@ namespace RenameTool
     {
         static string srcProjectPath; // 原项目路径
         static string srcProjectName; // 原项目名称
+        static string srcProjectNameRemarks; // 原项目名称备注（中文名）
+        static string srcProjectNameFirstCharLowercase; // 原项目名称首字母小写
+        static string srcProjectNameAllCharLowercase; // 原项目名称首字母全小写
+
         static string newProjectPath; // 新项目路径
         static string newProjectName; // 新项目名称
+        static string newProjectNameRemarks; // 原项目名称备注（中文名）
+        static string newProjectNameFirstCharLowercase; // 新项目名称首字母小写
+        static string newProjectNameAllCharLowercase; // 新项目名称首字母全小写
 
         static Configuration config;
-        static Regex replaceRegex;
+        //原项目名替换正则
+        static Regex replaceRegexOriginalProjectName;
+        //原项目备注（中文名）替换正则
+        static Regex replaceRegexProjectNameRemarks;
+        //原项目名首字母小写替换正则
+        static Regex replaceRegexFirstCharLowercase;
+        //原项目全小写替换正则
+        static Regex replaceRegexAllCharLowercase;
 
         static void Main(string[] args)
         {
+
             Console.Title = "项目重命名工具";
 
             config = Configuration.Build();
 
             srcProjectPath = CmdReader.ReadLine("请输入原项目路径：", input => Directory.Exists(input));
             srcProjectName = CmdReader.ReadLine("请输入原项目名称：");
+            srcProjectNameRemarks = CmdReader.ReadLine("请输入原项目中文名称：");
+            srcProjectNameFirstCharLowercase = srcProjectName.FirstCharToLower();
+            srcProjectNameAllCharLowercase = srcProjectName.ToLower();
 
             newProjectPath = CmdReader.ReadLine("请输入新项目保存位置：", input => Directory.Exists(input));
             newProjectName = CmdReader.ReadLine("请输入新项目名称：");
+            newProjectNameRemarks = CmdReader.ReadLine("请输入新项目中文名：");
+            newProjectNameFirstCharLowercase = newProjectName.FirstCharToLower();
+            newProjectNameAllCharLowercase = newProjectName.ToLower();
 
             newProjectPath = Path.Combine(newProjectPath, newProjectName);
 
             Console.WriteLine("正在处理...");
 
-            replaceRegex = new Regex(
-                srcProjectName,
-                RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled);
+            replaceRegexOriginalProjectName = new Regex(
+                srcProjectName, RegexOptions.Multiline | RegexOptions.Compiled);
+            replaceRegexProjectNameRemarks = new Regex(
+               srcProjectNameRemarks, RegexOptions.Multiline | RegexOptions.Compiled);
+            replaceRegexFirstCharLowercase = new Regex(
+                srcProjectName.FirstCharToLower(), RegexOptions.Multiline | RegexOptions.Compiled);
+            replaceRegexAllCharLowercase = new Regex(
+                srcProjectName.ToLower(), RegexOptions.Multiline | RegexOptions.Compiled);
 
             // 保存到桌面
             // Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
@@ -86,9 +112,13 @@ namespace RenameTool
         {
             Directory.CreateDirectory(Path.GetDirectoryName(destFile));
 
-            var encoding = Util.GetEncoding(srcFile);
+            //var encoding = Util.GetEncoding(srcFile);
+            var encoding = Encoding.UTF8;
             var srcText = File.ReadAllText(srcFile, encoding);
-            var destText = replaceRegex.Replace(srcText, newProjectName);
+            var destText = replaceRegexOriginalProjectName.Replace(srcText, newProjectName);
+            destText = replaceRegexProjectNameRemarks.Replace(destText, newProjectNameRemarks);
+            destText = replaceRegexFirstCharLowercase.Replace(destText, newProjectNameFirstCharLowercase);
+            destText = replaceRegexAllCharLowercase.Replace(destText, newProjectNameAllCharLowercase);
 
             File.WriteAllText(destFile, destText, encoding);
         }
